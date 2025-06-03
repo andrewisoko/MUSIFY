39ed8eef505ccd3bc6dd517ab1507bbc7b61fb02
andrewisokoe <andrewisoko1998@gmail.com>
2025-06-03 18:15:40 +0100

    Restructuring the src directory. fixing all bugs.

--- src/main.py
+++ src/main.py
@@ -1,43 +1,19 @@
+
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
 from oauth_spotify import OAuth_Spotify
-import os
-import urllib.parse
-from fastapi.responses import Response
-from dotenv import load_dotenv
+import uvicorn
 
 
-
-app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key= "to be added")
+app = FastAPI(middleware=[Middleware(SessionMiddleware, secret_key="to be added")])
 oauth = OAuth_Spotify()
-oauth.spotify_urls()
 
-
-@app.get("/")
-async def frontend_msg():
-       content = "Welcome to Musify<br>Please click the link to log in <a href='/login'>Spotify Log In</a>"
-       return Response(content=content, media_type="text/html")
-
-@app.get("/login")
-async def login():
-    
-    
-    load_dotenv(dotenv_path="src/.env")
-    
-    scope = 'user-read-private user-read-email'
-    params = {
-        "client_id": os.getenv("CLIENT_ID"),
-        "response_type": "code",
-        "scope": scope,
-        "redirect_uri": oauth.redirect_uri,
-        "show_dialog": True
-    }
-    auth_url = f"{oauth.auth_url}?{urllib.parse.urlencode(params)}"
-    return RedirectResponse(auth_url)
-
+app.add_api_route("/", oauth.home, methods=["GET"])
+app.add_api_route("/login", oauth.login, methods=["GET"])
+app.add_api_route("/callback", oauth.callback, methods=["GET"])
+app.add_api_route("/playlists", oauth.get_playlists, methods=["GET"])
+app.add_api_route("/refresh-token", oauth.refresh_token, methods=["GET"])
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
--- src/oauth_spotify.py
+++ src/oauth_spotify.py
@@ -1,83 +1,98 @@
-from fastapi import FastAPI,Request
-from fastapi.responses import RedirectResponse,JSONResponse
+from fastapi import Request
+from fastapi.responses import Response, RedirectResponse, JSONResponse
 import requests
 import os
 from datetime import datetime
-from starlette.middleware.sessions import SessionMiddleware
+import urllib.parse
 from dotenv import load_dotenv
 
 
 
-class OAuth_Spotify():
-    
-   
-    
-    app = FastAPI()
 
-        
-    def spotify_urls(self):
-        
-        self.url_account_apitoken ="https://accounts.spotify.com/api/token"
+class OAuth_Spotify:
+    
+    """all the accessible urls are contanied in the contstructor."""
+    
+    def __init__(self):
+        self.url_account_apitoken = "https://accounts.spotify.com/api/token"
         self.redirect_uri = "http://127.0.0.1:8888/callback"
-        self.auth_url ="https://accounts.spotify.com/authorize"
-        self.baseapi_url ="https://api.spotify.com/v1/"
+        self.auth_url = "https://accounts.spotify.com/authorize"
+        self.baseapi_url = "https://api.spotify.com/v1/"
+    
+    
+    async def home(self):
         
-    
-    
-    @app.route("/callback")
-    
-    async def callback(self):
+        """The first page from local address, provides the spotify login page link"""
         
-        request = Request()
+        content = "Welcome to Musify<br>Please click the link to log in <a href='/login'>Spotify Log In</a>"
+        return Response(content=content, media_type="text/html")
+    
+    
+
+    async def login(self, request: Request):
+        
+        """Redirect the user to the auth url."""
+        
         load_dotenv(dotenv_path="src/.env")
+        scope = 'user-read-private user-read-email'
+        params = {
+            "client_id": os.getenv("CLIENT_ID"),
+            "response_type": "code",
+            "scope": scope,
+            "redirect_uri": self.redirect_uri,
+            "show_dialog": True
+        }
+        auth_url = f"{self.auth_url}?{urllib.parse.urlencode(params)}"
+        return RedirectResponse(auth_url)
+    
+    
+
+    async def callback(self, request: Request):
         
+        """Post recquest to the api token url and redirecting to playlist endpoint."""
+        
+        load_dotenv(dotenv_path="src/.env")
         if 'error' in request.query_params:
-            return {"error":request.query_params['error']}
+            return {"error": request.query_params['error']}
         
         elif 'code' in request.query_params:
             request_body = {
                 'code': request.query_params['code'],
                 'grant_type': 'authorization_code',
                 'redirect_uri': self.redirect_uri,
-                'client_id':  os.getenv("CLIENT_ID"),
+                'client_id': os.getenv("CLIENT_ID"),
                 'client_secret': os.getenv("CLIENT_SECRET"),  
             }
-            
             response = requests.post(self.url_account_apitoken, data=request_body)
-            token_json_info= response.json()
-            
+            token_json_info = response.json()
             request.session['access_token'] = token_json_info['access_token']
-            request.session['refresh_token']  = token_json_info['refresh_token']
+            request.session['refresh_token'] = token_json_info['refresh_token']
             request.session['expires_at'] = datetime.now().timestamp() + token_json_info['expires_in']
-            
+    
             return RedirectResponse('/playlists')
-    
-    
-     
-     
-    @app.route('/playlists')       
-            
-    async def get_playlists(self,session : Request):
-        
-        if 'access_token' not in session:
-            return RedirectResponse(url="/login")
 
-        elif datetime.now().timestamp() > session.get('expires_at', 0):
+
+
+    async def get_playlists(self, request: Request):
+        
+        """Playlist displayed as json document."""
+        
+        if 'access_token' not in request.session:
+            return RedirectResponse(url="/login")
+        
+        elif datetime.now().timestamp() > request.session.get('expires_at', 0):
             return RedirectResponse(url="/refresh-token")
         
-        headers = {
-        'Authorization': f"Bearer {session['access_token']}"
-    }
+        headers = {'Authorization': f"Bearer {request.session['access_token']}"}
         response = requests.get(self.baseapi_url + 'me/playlists', headers=headers)
         playlists = response.json()
-        
         return JSONResponse(content=playlists)
-    
-    
 
-    @app.route('/refresh-token')       
-            
-    async def refresh_token(self,request: Request):  
+
+
+    async def refresh_token(self, request: Request):
+        
+        """Handles expired refrsh token by providing new tokens if the latter is expired, for then redirecting to the playlist endpoint."""
         
         refresh_token = request.session.get('refresh_token')
         
@@ -85,26 +100,14 @@ class OAuth_Spotify():
             return RedirectResponse("/login")
         
         elif datetime.now().timestamp() > request.session.get('expires_at', 0):
-            
             request_body = {
-        
                 'grant_type': 'refresh_token',
                 'refresh_token': request.session['refresh_token'],
-                'client_id':  os.getenv("CLIENT_ID"),
+                'client_id': os.getenv("CLIENT_ID"),
                 'client_secret': os.getenv("CLIENT_SECRET"),  
             }
-            
-            response = requests.post(url=self.url_account_apitoken,data=request_body)
+            response = requests.post(url=self.url_account_apitoken, data=request_body)
             new_token_json_info = response.json()
-            
             request.session["access_token"] = new_token_json_info["access_token"]
             request.session['expires_at'] = datetime.now().timestamp() + new_token_json_info['expires_in']
-            
-            
             return RedirectResponse("/playlists")
-        
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)     
-        
