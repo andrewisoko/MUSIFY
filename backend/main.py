7082a915ca3676db38a1505d3923fd211765f92f
andrewisokoe <andrewisoko1998@gmail.com>
2025-06-14 14:58:41 +0100

    Add Home.jsx component

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,27 @@
 
 from fastapi import FastAPI
 from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from oauth_spotify import OAuth_Spotify
 import uvicorn
 
 
-app = FastAPI(middleware=[Middleware(SessionMiddleware, secret_key=b"add key.")])
+
 oauth = OAuth_Spotify()
+
+origins = [
+    "http://localhost:5173/"
+           ]
+
+app = FastAPI(middleware=[Middleware(SessionMiddleware, secret_key=b"add key.")])
+app.add_middleware(CORSMiddleware,
+                   allow_origins = origins,
+                   allow_credentials = True,
+                   allow_methods = ["*"],
+                   allow_headers = ["*"],
+                   )
+
 
 app.add_api_route("/", oauth.home, methods=["GET"])
 app.add_api_route("/login", oauth.login, methods=["GET"])
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import "../css/Home.css"
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Musify</h1>
+        <p>
+          <strong>Playlists And Audio Downloader</strong>
+          </p>
+          <p className="home-texts">Log in to your Spotify account to download your favourite playlist or insert url to download audio.</p>
+          <button className="spotify-login-btn">Login With Spotify</button>
+            <div class="download-bar">
+                <input type="text" class="download-input" placeholder="Enter a URL"/>
+                <button class="download-btn" aria-label="Download">
+                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none">
+                    <path d="M11 3v10" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M7 11l4 4 4-4" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <rect x="5" y="17" width="12" height="2" rx="1" fill="#fff"/>
+                    </svg>
+                </button>
+            </div>
+            
+
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+};
+
+export default Home
