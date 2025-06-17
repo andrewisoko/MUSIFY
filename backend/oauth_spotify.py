e70f6fe092e859a6823c9794b9aa81411d844331
andrewisokoe <andrewisoko1998@gmail.com>
2025-06-17 14:14:02 +0100

    Substitute request method with httpx to get faster api responses.

--- backend/oauth_spotify.py
+++ backend/oauth_spotify.py
@@ -1,6 +1,7 @@
 from fastapi import Request
 from fastapi.responses import Response, RedirectResponse, JSONResponse
-import requests
+import httpx
+import asyncio
 import os
 from datetime import datetime
 import urllib.parse
@@ -10,17 +11,23 @@ import json
 
 
 
+
 class OAuth_Spotify:
     
     """all the accessible urls are contanied in the contstructor."""
     
     def __init__(self):
+        
+        load_dotenv(".env")
         self.url_account_apitoken = "https://accounts.spotify.com/api/token"
         self.redirect_uri = "http://127.0.0.1:8888/callback"
         self.auth_url = "https://accounts.spotify.com/authorize"
         self.baseapi_url = "https://api.spotify.com/v1/me/playlists"
         self.url_getplaylist = None
         self.playlists = None
+        self.client_id = os.getenv("CLIENT_ID")
+        self.client_secret = os.getenv("CLIENT_SECRET")
+        
     
     
     async def home(self):
@@ -39,7 +46,7 @@ class OAuth_Spotify:
         load_dotenv(".env")
         scope = 'playlist-read-collaborative'
         params = {
-            "client_id": os.getenv("CLIENT_ID"),
+            "client_id":f"{self.client_id}",
             "response_type": "code",
             "scope": scope,
             "redirect_uri": self.redirect_uri,
@@ -54,7 +61,7 @@ class OAuth_Spotify:
         
         """Post recquest to the api token url and redirecting to playlist endpoint."""
         
-        load_dotenv(".env")
+        
         if 'error' in request.query_params:
             return {"error": request.query_params['error']}
         
@@ -63,14 +70,15 @@ class OAuth_Spotify:
                 'code': request.query_params['code'],
                 'grant_type': 'authorization_code',
                 'redirect_uri': self.redirect_uri,
-                'client_id': os.getenv("CLIENT_ID"),
-                'client_secret': os.getenv("CLIENT_SECRET"),  
+                'client_id':f"{self.client_id}",
+                'client_secret':f"{self.client_secret}",  
             }
-            response = requests.post(self.url_account_apitoken, data=request_body)
-            token_json_info = response.json()
-            request.session['access_token'] = token_json_info['access_token']
-            request.session['refresh_token'] = token_json_info['refresh_token']
-            request.session['expires_at'] = datetime.now().timestamp() + token_json_info['expires_in']
+            async with httpx.AsyncClient() as client:
+                response = await client.post(self.url_account_apitoken, data=request_body)
+                token_json_info = response.json()
+                request.session['access_token'] = token_json_info['access_token']
+                request.session['refresh_token'] = token_json_info['refresh_token']
+                request.session['expires_at'] = datetime.now().timestamp() + token_json_info['expires_in']
             
             return RedirectResponse('http://127.0.0.1:5173/playlists')
 
@@ -88,8 +96,9 @@ class OAuth_Spotify:
             return RedirectResponse(url="/refresh-token")
         
         headers = {'Authorization': f"Bearer {request.session['access_token']}"}
-        response = requests.get(self.baseapi_url, headers=headers,params={"limit": 50})
-        self.playlists = response.json()
+        async with httpx.AsyncClient() as client:
+            response = await client.get(self.baseapi_url, headers=headers,params={"limit": 50})
+            self.playlists = response.json()
         return JSONResponse(content=self.playlists)
     
     
@@ -112,47 +121,45 @@ class OAuth_Spotify:
             # Request to access a playlist based on its id
     
             headers = {f"Authorization": f"Bearer {request.session["access_token"]}"}
-            playlist_recquest = requests.get(url=self.url_getplaylist, headers=headers)
-    
-            single_playlist = playlist_recquest.json()
-            playlists_items.pop(playlist_dict_index)
-    
-    
-            track_info = []
-            track_items = single_playlist["tracks"]["items"]
-            tot_tracks_items = len(single_playlist["tracks"]["items"])
-    
-            track_index = 0
-    
-            while track_index < tot_tracks_items:
+            async with httpx.AsyncClient() as client:
+                playlist_recquest = await client.get(url=self.url_getplaylist, headers=headers)
+        
+                single_playlist = playlist_recquest.json()
+                playlists_items.pop(playlist_dict_index)
+        
+        
+                track_info = []
+                track_items = single_playlist["tracks"]["items"]
+                tot_tracks_items = len(single_playlist["tracks"]["items"])
+        
+                track_index = 0
+        
+                while track_index < tot_tracks_items:
+                    
+                    track_dict = track_items[track_index]
+                    artist_name = track_dict["track"]["album"]["artists"][0]["name"]
+                    track_name = track_dict["track"]["name"]
+                    
+                    track_info.append(f'{artist_name} {track_name}')
+                    
+                    track_index += 1
+                json_trial_list.append({playlist_name:track_info})
+                    
+            with open("all_tracks.json","w") as file:
+                json.dump(json_trial_list,file,indent=4)
                 
-                track_dict = track_items[track_index]
-                artist_name = track_dict["track"]["album"]["artists"][0]["name"]
-                track_name = track_dict["track"]["name"]
-                
-                track_info.append(f'{artist_name} {track_name}')
-                
-                track_index += 1
-            json_trial_list.append({playlist_name:track_info})
-                
-        with open("all_tracks.json","w") as file:
-            json.dump(json_trial_list,file,indent=4)
-            
-        with open("all_tracks.json","r") as read_file:
-            all_tracks_file = json.load(read_file)
+            with open("all_tracks.json","r") as read_file:
+                all_tracks_file = json.load(read_file)
             
         return JSONResponse(content=all_tracks_file)
      
         
         
         
-
-
     async def refresh_token(self, request: Request):
         
         """Handles expired refrsh token by providing new tokens if the latter is expired, for then redirecting to the playlist endpoint."""
         
-        load_dotenv(".env")
         refresh_token = request.session.get('refresh_token')
         
         if not refresh_token:
@@ -162,12 +169,14 @@ class OAuth_Spotify:
             request_body = {
                 'grant_type': 'refresh_token',
                 'refresh_token': request.session['refresh_token'],
-                'client_id': os.getenv("CLIENT_ID"),
-                'client_secret': os.getenv("CLIENT_SECRET"),  
+                'client_id':f"{self.client_id}",
+                'client_secret':f"{self.client_secret}",  
             }
-            response = requests.post(url=self.url_account_apitoken, data=request_body)
-            new_token_json_info = response.json()
-            request.session["access_token"] = new_token_json_info["access_token"]
-            request.session['expires_at'] = datetime.now().timestamp() + new_token_json_info['expires_in']
+            async with httpx.AsyncClient() as client:
+                
+                response = await client.post(url=self.url_account_apitoken, data=request_body)
+                new_token_json_info = response.json()
+                request.session["access_token"] = new_token_json_info["access_token"]
+                request.session['expires_at'] = datetime.now().timestamp() + new_token_json_info['expires_in']
             return RedirectResponse("/playlists")
         
