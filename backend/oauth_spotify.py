45c6adb051105573a5c18e9db79eab3a3c352099
andrewisokoe <andrewisoko1998@gmail.com>
2025-06-20 11:20:22 +0100

    Retrieving data tracks data from the backend.

--- backend/oauth_spotify.py
+++ backend/oauth_spotify.py
@@ -6,7 +6,8 @@ from datetime import datetime
 import urllib.parse
 from dotenv import load_dotenv
 import json
-
+import time
+import requests
 
 
 
@@ -26,7 +27,7 @@ class OAuth_Spotify:
         self.playlists = None
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
-        
+    
     
     
     async def home(self):
@@ -98,60 +99,37 @@ class OAuth_Spotify:
         async with httpx.AsyncClient() as client:
             response = await client.get(self.baseapi_url, headers=headers,params={"limit": 50})
             self.playlists = response.json()
+           
         return JSONResponse(content=self.playlists)
     
     
 
-    async def get_tracks(self,request:Request):
-        
-        """Returns json file dictionary with all playlist and songs.""" 
-        
+    async def get_tracks(self, request: Request):
         playlists_data = self.playlists
         json_trial_list = []
-        playlists_items = playlists_data["items"] #list
-        playlist_dict_index = 0
-
-        for playlist_amount in range(len(playlists_items)):
-    
-            playlist_name = playlists_items[0]["name"]
-            playlists_id = playlists_items[0]["id"]
+        for playlist in playlists_data["items"]:
+            playlist_name = playlist["name"]
+            playlists_id = playlist["id"]
             self.url_getplaylist = f"https://api.spotify.com/v1/playlists/{playlists_id}"
-    
-            # Request to access a playlist based on its id
-    
-            headers = {f"Authorization": f"Bearer {request.session["access_token"]}"}
+
+            headers = {"Authorization": f"Bearer {request.session['access_token']}"}
             async with httpx.AsyncClient() as client:
-                playlist_recquest = await client.get(url=self.url_getplaylist, headers=headers)
-        
-                single_playlist = playlist_recquest.json()
-                playlists_items.pop(playlist_dict_index)
-        
-        
-                track_info = []
-                track_items = single_playlist["tracks"]["items"]
-                tot_tracks_items = len(single_playlist["tracks"]["items"])
-        
-                track_index = 0
-        
-                while track_index < tot_tracks_items:
-                    
-                    track_dict = track_items[track_index]
-                    artist_name = track_dict["track"]["album"]["artists"][0]["name"]
-                    track_name = track_dict["track"]["name"]
-                    
-                    track_info.append(f'{artist_name} {track_name}')
-                    
-                    track_index += 1
-                json_trial_list.append({playlist_name:track_info})
-                    
-            with open("all_tracks.json","w") as file:
-                json.dump(json_trial_list,file,indent=4)
-                
-            with open("all_tracks.json","r") as read_file:
-                all_tracks_file = json.load(read_file)
-            
-        return JSONResponse(content=all_tracks_file)
-     
+                playlist_request = await client.get(url=self.url_getplaylist, headers=headers)
+                single_playlist = playlist_request.json()
+
+            track_info = []
+            track_items = single_playlist["tracks"]["items"]
+            for track_dict in track_items:
+                artist_name = track_dict["track"]["album"]["artists"][0]["name"]
+                track_name = track_dict["track"]["name"]
+                track_info.append(f'{artist_name} {track_name}')
+            json_trial_list.append({playlist_name: track_info})
+
+        with open("all_tracks.json", "w") as file:
+            json.dump(json_trial_list, file, indent=4)
+
+        return JSONResponse(content=json_trial_list)
+
         
         
         
--- frontend/src/pages/PlaylistCards.jsx
+++ frontend/src/pages/PlaylistCards.jsx
@@ -1,21 +1,37 @@
+import React, { useEffect, useState } from "react";
 import "../css/PlaylistCards.css";
 import folder from "../assets/folder.png";
 import download from "../assets/download.png";
+import api from "../services/api"
 
-function PlaylistCards({ pl_item }) {
+
+
+function PlaylistCards({ pl_item: plItem }) {
+
+  const allTracks = api.get("/tracks")
+  
+  function playlistName() {
+
+    if (plItem.name){
+      console.log("YAH MAN")
+    }else{
+      console.log("Not here")
+    }
+  }
+
   return (
     <div className="pl-card">
       <div className="pl-poster">
-        <img src={pl_item.url} alt={pl_item.name} />
+        <img src={plItem.url} alt={plItem.name} />
         <div className="download-overlay">
-          <button className="download-icon">
+          <button className="download-icon" onClick={playlistName}>
             <img src={download} style={{ width: '30px', height: '30px'}}alt="download" />
           </button>
         </div>
       </div>
-      <div className="pl-title">{pl_item.name}</div>
+      <div className="pl-title">{plItem.name}</div>
       <div className="pl-songs">
-        <span className="song-count">{pl_item.tracks} song/s</span>
+        <span className="song-count">{plItem.tracks} song/s</span>
         <img src={folder} className="folder-style" alt="folder" />
       </div>
     </div>
