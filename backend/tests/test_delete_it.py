50f80ebae6b70bfa77abd57235cac7e3dbfe71c2
andrewisokoe <andrewisoko1998@gmail.com>
2025-07-07 17:39:50 +0100

    Adjusting the last issue with test_delete_it.py

--- backend/tests/test_delete_it.py
+++ backend/tests/test_delete_it.py
@@ -4,14 +4,17 @@ import zipfile
 import pytest
 from pathlib import Path
 
-from backend.delete_it import DeleteIt  
+from backend.delete_it import DeleteIt 
+
+
 
 @pytest.fixture
 def fake_filesystem():
     with tempfile.TemporaryDirectory() as tempdir:
         
         """Creating an emulation directory with all the necesary file paths."""
-
+        
+        pre_tempdir = os.path.dirname(tempdir)
         
         plst_downloads = os.path.join(tempdir, "Playlists downloads", "playlist1", "downloads")
         zip_playlist_dir = os.path.join(tempdir, "zip playlist")
@@ -19,11 +22,10 @@ def fake_filesystem():
         yt_downloads = os.path.join(tempdir, "Youtube Downloads", "downloads")
         zip_audio_path = os.path.join(tempdir, "Youtube downloads.zip")
         playlists_cache = os.path.join(tempdir, "playlists_cache.json")
-        all_tracksjson = os.path.join(tempdir, "frontend", "public", "services","allTracks.json")
+        all_tracksjson = os.path.join(pre_tempdir, "frontend", "public", "services","allTracks.json")
         
 
       
-        # os.makedirs(frontend_dir, exist_ok=True)
         os.makedirs(plst_downloads)
         os.makedirs(zip_playlist_dir, exist_ok=True)
         os.makedirs(yt_downloads)
@@ -42,7 +44,7 @@ def fake_filesystem():
         with zipfile.ZipFile (zip_audio_path,"w") as audiozip:
             audiozip.writestr(data="audio dummy.txt", zinfo_or_arcname = "Youtube downloads.zip")
             
-            
+        os.makedirs(os.path.dirname(all_tracksjson), exist_ok=True)    
         with open(all_tracksjson, "w") as f:
             f.write("{}")
  
@@ -53,21 +55,15 @@ def fake_filesystem():
 
 
 
-
-
     
-def test_delete_all(monkeypatch, fake_filesystem):
+def test_delete_all(monkeypatch, fake_filesystem): #retrieving the fake file by passing the pytest.fixture as a parameter
    
     dummy_file = os.path.join(fake_filesystem, "dummy_script.py") #dummy_script.py is the equivalent of __file__ on the os.path.abspath function
     with open(dummy_file, "w") as f:
         f.write("dummy")
     monkeypatch.setattr("os.path.abspath", lambda x =None: dummy_file) # this will result as os.path.abspath(__file__) in the delete_it module
-    
-    dummy_file_path = (os.path.dirname(dummy_file))
-    temp_path = Path(dummy_file_path).resolve().parent
-    
- 
-    deleter = DeleteIt()
-    result = deleter.delete_all()
-    assert result == "All generated items deleted."
+   
+    cleanup = DeleteIt()
+    result = cleanup.delete_all()
+    assert result == "All generated items deleted." 
 
